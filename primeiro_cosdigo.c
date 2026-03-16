bd6c3313eb0247ad4bb2dab4590514d0266ee117
Anderson Costa <costaandersom23@gmail.com>
2026-03-16 11:32:52 -0300

    comit inicial

---
+++ primeiro_cosdigo.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Nesta linha importamos a biblioteca que permite que tenhamos entrada e saida de dados no C
+
+// o main e a primeira função que sera executada dentro do nosso codigo, dentro dela vamos definir o que sera executado
+int main(){
+
+    // O printf e uma função que imprime na tela o que for descrito dentro da () com aspas duplas "" o \n serve para quebrar linha 
+    printf("Ola mundo\n");
+    
+    return 0; // todda função tem que ter um return obrigatorio
+}
