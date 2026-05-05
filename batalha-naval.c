056b84a45654f7de37c55a8b88f690f421e4a90c
Anderson Costa <costaandersom23@gmail.com>
2026-05-05 11:56:21 -0300

    Refactor naval battle game to use constants for board dimensions and improve code readability

--- batalha-naval.c
+++ batalha-naval.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define COLUNA 10
+#define LINHA 10
+
 int main() {
 
     // criação da tabela bidimensional de 10x10
@@ -16,21 +19,21 @@ int main() {
     //     {0,0,0,0,0,0,0,0,0,0}  // Linha 10
     // }; o codigo abaixo faz esta tabela de forma automatica sem escrita manual 
     
-    int tabuleiro[10][10];
-    for(int i = 0 ; i < 10 ; i++){
-        for(int e = 0 ; e < 10 ; e++){
+    int tabuleiro[LINHA][COLUNA];
+    for(int i = 0 ; i < LINHA ; i++){
+        for(int e = 0 ; e < COLUNA ; e++){
             tabuleiro[i][e] = 0;
         }
     }
 
     // define o navio na horizontal
     // ==========================================
-    // printf("Defina o inicio da embarcao\n");
+    printf("Defina o inicio da embarcao\n");
     int posx = 2, posy = 4;
-    // printf("Posicao x: ");
-    // scanf("%d", &posx);
-    // printf("\nPosicao: ");
-    // scanf("%d", &posy);
+    printf("Posicao x: ");
+    scanf("%d", &posx);
+    printf("\nPosicao y : ");
+    scanf("%d", &posy);
     
     for(int i = posy ; i < posy+3 ; i++){
         tabuleiro[posx][i] = 1;
@@ -38,21 +41,51 @@ int main() {
 
     // define o navio na vertical 
     // ==========================================
-    // printf("Defina o inicio da embarcao\n");
+    printf("Defina o inicio da embarcao\n");
     posx = 7;
     posy = 2;
-    // printf("Posicao x: ");
-    // scanf("%d", &posx);
-    // printf("\nPosicao: ");
-    // scanf("%d", &posy);
+    printf("Posicao x: ");
+    scanf("%d", &posx);
+    printf("\nPosicao y: ");
+    scanf("%d", &posy);
 
     for(int i = posx ; i < posx+3 ; i++){
         tabuleiro[i][posy] = 1;
     };
+
+
+    // define os navios na diagonal
+    // ==========================================
+
+    printf("Defina o inicio da embarcação\n");
+    posx = 1;
+    posy = 1;
+    printf("Posicao x: ");
+    scanf("%d", &posx);
+    printf("\nPosicao y: ");
+    scanf("%d", &posy);
+
+    for(int i = 0 ; i < posx+3; i++){
+        tabuleiro[posx + i][posy + i] = 1;
+    };
+
+
+    printf("Defina o inicio da embarcação\n");
+    posx = 1;
+    posy = 1;
+    printf("Posicao x: ");
+    scanf("%d", &posx);
+    printf("\nPosicao y: ");
+    scanf("%d", &posy);
+
+    for(int i = 0 ; i < posx+3; i++){
+        tabuleiro[posx + i][posy - i] = 1;
+    };
     
     // Printar a tabela no terminal
-    for(int i = 0 ; i < 10 ; i++) {
-        for(int e = 0 ; e < 10 ; e++) {
+    // ==========================================
+    for(int i = 0 ; i < LINHA ; i++) {
+        for(int e = 0 ; e < COLUNA ; e++) {
             printf("%d ", tabuleiro[i][e]);
         };
         printf("\n");
